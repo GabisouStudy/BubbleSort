6b11e77bc6aadd252befb807b6ee8f181c23c258
Gabriel <cladangab@gmail.com>
2016-03-31 04:52:49 -0300

    Update 02

--- BubbleSort/Form1.cs
+++ BubbleSort/Form1.cs
@@ -18,117 +18,84 @@ namespace BubbleSort
         {
             InitializeComponent();
         }
-        
-        List<int> values1 = new List<int>();
+
+        async Task PutTaskDelay()
+        {
+            await Task.Delay(5000);
+        }
+
+        private int[] RandomArray(int size)
+        {
+            int[] array = new int[size];
+            Random r = new Random();
+            for (int i = 0; i < size; i++)
+                array[i] = (r.Next(0, 200));
+            return array;
+        }
+
+        private void UpdateChart(Chart c, int[] a, int c1, int c2) {
+            Chart chart = c;
+            int[] array = a;
+            chart.Series.Clear();
+            Series series = chart.Series.Add("");
+            chart.Series[0]["PointWidth"] = "0.5";
+            for (int i = 0; i < array.Length; i++)
+            {
+                chart.Series[0].Points.Add(array[i]);
+            }
+            if (c1 != c2) {
+
+                chart1.Series[0].Points[c1].Color = Color.Red;
+                chart1.Series[0].Points[c2].Color = Color.Red;
+            }
+        }
+
+        int[] values1;
 
         private void Form1_Load(object sender, EventArgs e)
         {
             this.chart1.Titles.Add("BubbleSort");
 
-            for (int i = 1; i < size1.Value; i++)
-            {
-                values1.Add(i);
-            }
+            values1 = RandomArray(int.Parse("" + (size1.Value + 1)));
 
-            chart1.Series.Clear();
-            Series series = chart1.Series.Add("Entrada Teste ");
-            chart1.Series[0]["PointWidth"] = "0.5";
-            for (int i = 1; i <= size1.Value; i++)
-            {
-                series.Points.Add(i);
-            }
+            UpdateChart(chart1, values1, 0, 0);
+
+            //Series series = chart.Series.Add("");
         }
 
         private void buttonSort1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("" + values1.Count);
-            values1.BubbleSort();
-
-            chart1.Series.Clear();
-            Series series = chart1.Series.Add("Entrada Teste ");
-            chart1.Series[0]["PointWidth"] = "0.5";
-
-            for (int i = 0; i < size1.Value; i++)
+            for (int i = 0; i < values1.Length - 1; i++)
             {
-                series.Points.Add(values1[i]);
+                for (int j = i + 1; j < values1.Length; j++)
+                {
+                    if (values1[i] > values1[j])
+                    {
+                        int temp = values1[i];
+                        values1[i] = values1[j];
+                        values1[j] = temp;
+                        chart1.Update();
+
+                        //await PutTaskDelay();
+                        //System.Threading.Thread.Sleep(100);
+                    }
+                    UpdateChart(chart1, values1, i, j);
+                    System.Threading.Thread.Sleep(500);
+                }
             }
+
         }
 
         private void size1_ValueChanged(object sender, EventArgs e)
         {
-            for (int i = 1; i < size1.Value + 1; i++)
-            {
-                values1.Add(i);
-            }
+            values1 = RandomArray(int.Parse("" + (size1.Value + 1)));
 
-            chart1.Series.Clear();
-            Series series = chart1.Series.Add("Entrada Teste ");
-            chart1.Series[0]["PointWidth"] = "0.5";
-
-            for (int i = 1; i <= size1.Value; i++){
-                series.Points.Add(i);
-            }
+            UpdateChart(chart1, values1, 0, 0);
         }
 
         private void buttonRandom1_Click(object sender, EventArgs e)
         {
-            for (int i = 1; i < size1.Value+1; i++)
-            {
-                values1.Add(i);
-            }
 
-            values1.Shuffle();
-
-            chart1.Series.Clear();
-            Series series = chart1.Series.Add("Entrada Teste ");
-            chart1.Series[0]["PointWidth"] = "0.5";
-
-            for (int i = 0; i < size1.Value; i++)
-            {
-                series.Points.Add(values1[i]);
-            }
         }
     }
-
-    static class MyExtensions{
-        private static Random rng = new Random();
-
-        public static void Shuffle<T>(this IList<T> list)
-        {
-            int n = list.Count;
-            while (n > 1)
-            {
-                n--;
-                int k = rng.Next(n + 1);
-                T value = list[k];
-                list[k] = list[n];
-                list[n] = value;
-            }
-        }
-
-        public static List<int> BubbleSort(this List<int> vetor)
-        {
-            int tamanho = vetor.Count;
-            int comparacoes = 0;
-            int trocas = 0;
-
-            for (int i = tamanho - 1; i >= 1; i--)
-            {
-                for (int j = 0; j < i; j++)
-                {
-                    comparacoes++;
-                    if (vetor[j] > vetor[j + 1])
-                    {
-                        int aux = vetor[j];
-                        vetor[j] = vetor[j + 1];
-                        vetor[j + 1] = aux;
-                        trocas++;
-                    }
-                }
-            }
-
-            return vetor;
-        }
-    }
-
 }
